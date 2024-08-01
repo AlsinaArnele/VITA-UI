3151d86aadfa3d7c7f1a1a2eb9cbc0a81e5909af
Alsina Arnele <[leviskibet2002@gmail.com]>
2024-08-02 02:54:50 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+    
+    function toggleHistory() {
+        const history = document.getElementById('history');
+        if (history.style.display === 'none' || history.style.display === '') {
+            history.style.display = 'flex';
+        } else {
+            history.style.display = 'none';
+        }
+    }
+
+    function responsivehistory() {
+        const history = document.getElementById('history');
+        if (history.style.display === 'none' || history.style.display === '') {
+            history.style.display = 'flex';
+            menuDisplay();
+        } else {
+            history.style.display = 'none';
+
+        }
+    }
+    
+    function profiletoggle() {
+        var profile = document.getElementById('profile');
+        var chat = document.getElementById('chat');
+        if(profile.style.display === 'none') {
+            chat.style.display = 'none';
+            profile.style.display = 'flex';
+        } else {
+            profile.style.display = 'none';
+            chat.style.display = 'flex';
+        }
+    }
+
+    function menuDisplay() {
+        var body = document.getElementById('body');
+        var menu = document.getElementById('nav');
+        var burger = document.getElementById('burger');
+        if(menu.style.display === 'none') {
+            menu.style.display = 'flex';
+            burger.style.display = 'none';
+        } else {
+            menu.style.display = 'none';
+            burger.style.display = 'block';
+        }
+    }
